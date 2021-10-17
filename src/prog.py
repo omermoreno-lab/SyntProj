2200ca8822337cc9509b891b9cbe8b6e46390ebe
omermoreno-lab <71211830+omermoreno-lab@users.noreply.github.com>
2021-10-17 13:53:40 +0300

    generate_examples is now generic

--- src/prog.py
+++ src/prog.py
@@ -48,22 +48,42 @@ def __list_pop__(l):
     return l
 
 
-def generate_examples():
+def __get_var_and_initializer(line: str, type_str_to_initializer: dict):
+    var, var_type = list(s.strip() for s in line.split(':'))
+    return var, type_str_to_initializer[var_type]
+
+
+def __get_randomized_environment(var_to_initializer: dict):
+    return {v: var_to_initializer[v]() for v in var_to_initializer}
+
+
+def generate_examples(program_path: str, env_setting_path: str):
     seed(SEED)
+    type_str_to_initializer = {"int": __free_int__, "str": __free_str__, "list[str]": lambda: __free_list__(__free_str__), "list[int]": lambda: __free_list__(__free_int__)}
+    # TODO: make str to initializer smarter, using reg expressions
+    with open(env_setting_path, 'r') as f:
+        var_to_initializer = dict(__get_var_and_initializer(line, type_str_to_initializer) for line in f)
 
-    examples = [[None, "x", "y", "n", "k", "d"]]
+    with open(program_path, 'r') as f:
+        program = f.read()
+
+    vars = list(var_to_initializer.keys)
+    vars.insert(0, None)
+    examples = [vars]       # first entry of examples is the 
     while len(examples) < NUM_EXAMPLES:
-        n = __free_int__()
-        k = __free_str__()
-        d = __free_list__()
+        exec(program, __get_randomized_environment(var_to_initializer))     # this will generate positive examples using the __inv__ call
 
-        x = y = 0
-        # while y < n and len(k) == 2:
-        while y < n:
-            examples.append([True, x, y, n, k, d])
-            x += 2
-            y += 1
-        # unreachable examples
-        # examples.append([False, x, y, n, k, d])
-    # examples.append([False, -1, -2, 5, 'hafhasf', [200,33]])
+    #     n = __free_int__()
+    #     k = __free_str__()
+    #     d = __free_list__()
+
+    #     x = y = 0
+    #     # while y < n and len(k) == 2:
+    #     while y < n:
+    #         examples.append([True, x, y, n, k, d])
+    #         x += 2
+    #         y += 1
+    #     # unreachable examples
+    #     # examples.append([False, x, y, n, k, d])
+    # # examples.append([False, -1, -2, 5, 'hafhasf', [200,33]])
     return examples
